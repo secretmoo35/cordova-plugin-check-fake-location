aaee389a50b8f1dd583f3ed9d4dd48392e4035b7
Sarayut Kungsaranuwat <sarayut.kungsaranuwat@gmail.com>
2020-12-24 14:20:20 +0700

    Update DetectFakeLocationPlugin.js

--- www/DetectFakeLocationPlugin.js
+++ www/DetectFakeLocationPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports.checkAllowMockLacation = function (arg0, success, error) {
-    exec(success, error, 'DetectFakeLocationPlugin', 'checkIsMockLocation', [arg0]);
+module.exports.checkAllowMockLacation = function (success, error) {
+  exec(success, error, 'DetectFakeLocationPlugin', 'checkIsMockLocation', []);
 };
